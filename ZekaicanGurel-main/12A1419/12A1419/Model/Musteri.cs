0bf0b1cea718c9b9bdfc43b6962ce7b234851c0b
ZekaicanGurel <zekaicangrl@gmail.com>
2024-12-09 12:39:57 +0300

    Add files via upload

--- ZekaicanGurel-main/12A1419/12A1419/Model/Musteri.cs
+++ ZekaicanGurel-main/12A1419/12A1419/Model/Musteri.cs
@@ -8,5 +8,19 @@ namespace _12A1419.Model
 {
     public class Musteri
     {
+        public int Id { get; set; } 
+
+        public string AdSoyad { get; set; }
+
+        public string FirmaAdi { get; set; }
+
+        public string Telefon { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Adres { get; set; }
+
+        public virtual ICollection<Satis> Satislar { get; set; } = new HashSet<Satis>();
+
     }
 }
--- ZekaicanGurel-main/12A1419/12A1419/Model/Personel.cs
+++ ZekaicanGurel-main/12A1419/12A1419/Model/Personel.cs
@@ -8,5 +8,29 @@ namespace _12A1419.Model
 {
     public class Personel
     {
+        //[Key]
+        public int Id { get; set; }
+
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+
+        public string Telefon { get; set; }
+
+        public string Mail{ get; set; }
+
+        public string Adres { get; set; }
+
+        public string KullanıcıAdı {  get; set; }
+
+        public string Sifre { get; set; }
+
+        //[ForeignKey("Yetki")]
+
+        public int YetkiId { get; set; }
+
+        public virtual ICollection<Satis> Satislar { get; set; }= new HashSet<Satis>();
+
+
     }
 }
